9e288a3663d10067eadea32f094d9ee73c3f39b3
Yudha Frebian <anandayudha777@gmail.com>
2024-11-23 17:34:14 +0700

    update content

--- script.js
+++ script.js
@@ -10,3 +10,17 @@ function changeLanguage(language, languageName, flagURL) {
     // Anda bisa menambahkan aksi lain di sini, seperti mengubah bahasa halaman
     console.log(`Bahasa yang dipilih: ${language}`);
 }
+
+const signToggle = document.getElementById("signToggle");
+const signSlide = document.getElementById("signWrapper");
+const signPage = document.getElementById("signPage");
+const loginPage = document.getElementById("loginPage");
+const welcomeWrapper = document.getElementById("welcomeWrapper");
+
+
+signToggle.addEventListener ("click", function() {
+    signSlide.style.transform = "translateX(-90%)";
+    welcomeWrapper.style.opacity = "0";
+    signPage.style.opacity = "1";
+    loginPage.style.opacity = "0";
+})
