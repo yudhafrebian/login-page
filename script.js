1b74892683f5dcbc6c7b7e415dcd5501a3acfd9e
yudhafrebian <anandayudha777@gmail.com>
2024-11-24 15:24:14 +0700

    update content

--- script.js
+++ script.js
@@ -16,11 +16,28 @@ const signSlide = document.getElementById("signWrapper");
 const signPage = document.getElementById("signPage");
 const loginPage = document.getElementById("loginPage");
 const welcomeWrapper = document.getElementById("welcomeWrapper");
-
+const loginWelcome = document.getElementById("welcomeLogin");
+const loginToggle = document.getElementById("loginToggle");
 
 signToggle.addEventListener ("click", function() {
     signSlide.style.transform = "translateX(-90%)";
     welcomeWrapper.style.opacity = "0";
+    welcomeWrapper.style.transitionDelay = "0s"
     signPage.style.opacity = "1";
+    signPage.style.transitionDelay ="1s";
     loginPage.style.opacity = "0";
+    loginPage.style.transitionDelay ="0s"
+    loginWelcome.style.transform = "translateX(0)";
+})
+
+loginToggle.addEventListener("click", function () {
+    signSlide.style.transform = "translateX(50%)";
+    welcomeWrapper.style.opacity = "1";
+    welcomeWrapper.style.transitionDelay = "1s"
+    signPage.style.opacity = "0";
+    signPage.style.transitionDelay ="0s"
+    loginPage.style.opacity = "1";
+    loginPage.style.transitionDelay = "1s"
+    loginWelcome.style.transform = "translateX(-200%)";
+    loginWelcome.style.transitionDelay = "0s"
 })
